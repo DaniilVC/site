5cc99ece1eff2b99769196d9bedf8af08dda2542
DaniilVC <daniilvercetti@outlook.com>
2026-04-20 12:59:24 +0300

    Добавил модели для расписания и судов, обновил связи между пользователями и их записями

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, TIMESTAMP, Enum
+from sqlalchemy import Column, Integer, String, TIMESTAMP, Enum, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from database import Base
 import enum
 
@@ -9,14 +10,59 @@ class UserRole(enum.Enum):
     director = "director"
     admin = "admin"
 
+class ScheduleStatus(str, enum.Enum):
+    unset = "Не задано"
+    mooring = "Швартовка"
+    unmooring = "Отшвартовка"
+    raid = "Рейд"
+
+class BerthNumber(str, enum.Enum):
+    unset = "Не задано"
+    berth1 = "Причал №1"
+    berth2 = "Причал №2"
+    berth3 = "Причал №3"
+    berth4 = "Причал №4"
+    berth5 = "Причал №5"
+
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    username = Column(String(60), unique=True, nullable=False)
+    username = Column(String(60), unique=False, nullable=False)
     telephone_number = Column(String(11), nullable=False, default="Отсутствует")
     email = Column(String(100), nullable=False)
     password = Column(String(255), nullable=False)
     role = Column(Enum(UserRole), default=UserRole.viewer)
     company = Column(String(100), default="Без компании")
     created_at = Column(TIMESTAMP, server_default=func.now())
+
+    #Связи
+    vessels = relationship("Vessel", back_populates="owner", cascade="all, delete-orphan")
+    schedule_entries = relationship("Schedule", back_populates="user", cascade="all, delete-orphan")
+
+
+class Vessel(Base):
+    __tablename__ = "vessels"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    vessel_name = Column(String(100), nullable=False, index=True)
+    vessel_number = Column(String(50), nullable=False)
+
+    #Связь
+    owner = relationship("User", back_populates="vessels")
+
+class Schedule(Base):
+    __tablename__ = "schedule"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False, index=True)
+    vessel_id = Column(Integer, ForeignKey("vessels.id", ondelete="CASCADE"), nullable=False)
+    date = Column(Date, nullable=False, index=True)
+    hour = Column(Integer, nullable=False, index=True)
+    status = Column(Enum(ScheduleStatus), default=ScheduleStatus.unset, nullable=False)
+    berth = Column(Enum(BerthNumber), default=BerthNumber.unset, nullable=False)
+    created_at = Column(TIMESTAMP, server_default=func.now())
+
+    user = relationship("User", back_populates="schedule_entries")
+    vessel = relationship("Vessel")
